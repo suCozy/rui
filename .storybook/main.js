cdd4506563f7c068fc810af70f77c92428120fc7
JeongminSung <hellojeongmin@gmail.com>
2022-09-05 13:53:54 +0900

    vite 설정 마무리

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const tsconfigPaths = require("vite-tsconfig-paths").default;
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  staticDirs: ["../public"],
+  core: {
+    builder: "@storybook/builder-vite", // 👈  vite 사용을 위해 해당 옵션을 추가합니다.
+  },
+  /**
+   * A option exposed by storybook-builder-vite for customizing the Vite config.
+   * @see https://github.com/eirslett/storybook-builder-vite#customize-vite-config
+   * @param {import("vite").UserConfig} config
+   * @see https://vitejs.dev/config/
+   */
+  viteFinal: async (config) => {
+    config.plugins.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+      })
+    );
+    return config;
+  },
+};
