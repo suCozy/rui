792511fd29f0568d3cbd7697b5fd39436b896c88
JeongminSung <hellojeongmin@gmail.com>
2022-09-05 14:01:47 +0900

    color 추가

---
+++ src/GlobalStyle/color.ts
@@ -0,0 +1,26 @@
+/**
+ * new color 추가 요망
+ * @see https://www.figma.com/file/4GdrEr4Xhpit0R4QCu4iEm/%EC%95%B1-%EB%82%B4-BI-%EA%B0%9C%ED%8E%B8?node-id=1%3A14758
+ */
+
+export const contents000 = "#212322";
+export const contents100 = "#646464";
+export const contents150 = "#8c8c8c";
+export const contents200 = "#aeaeae";
+export const contents300 = "#cccccc";
+export const contents999 = "#ffffff";
+
+// Disable disabled CTA
+export const disable = "#c7c7cd";
+export const disableAccent100 = "#ffbca6";
+export const disableAccent200 = "#a6c4ff";
+
+// Accent Error/ Link/
+export const accent100 = "#ea5128";
+export const accent200 = "#2864f0";
+export const background100 = "#ffffff";
+export const background200 = "#f5f5f5";
+export const background300 = "#ebebeb";
+export const backgroundAccent100 = "#faece8";
+export const backgroundAccent200 = "#eaf0fe";
+export const backgroundNew100 = "#fff9e5";
