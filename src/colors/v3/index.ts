271ba4c4436b69a111d8bf33e380b1d3c89423b1
k.jeong <kwanwoo.jeong@gmail.com>
2022-09-16 09:29:42 +0900

    PCWEB-8492 colors 모듈 추가

---
+++ src/colors/v3/index.ts
@@ -0,0 +1,32 @@
+/**
+ * {@link https://www.figma.com/file/4GdrEr4Xhpit0R4QCu4iEm/%EC%95%B1-%EB%82%B4-BI-%EA%B0%9C%ED%8E%B8?node-id=2%3A13145}
+ */
+
+export const primary100 = '#000000';
+export const primary200 = '#FFFFF9';
+
+export const secondary100 = '#FF5414';
+
+export const contents000 = '#212322';
+export const contents100 = '#61615F';
+export const contents150 = '#8B8B87';
+export const contents200 = '#B2B1AD';
+export const contents300 = '#CDCCC6';
+export const contents999 = '#FFFFF9';
+
+export const disable = '#CDCCC6';
+export const disableAccent100 = '#FFAA8D';
+export const disableAccent200 = '#FFCBBA';
+
+export const accent100 = '#BB3811';
+
+export const background100 = '#FFFFFF';
+export const background200 = '#F7F7F0';
+export const background300 = '#E8E7E0';
+
+export const backgroundAccent100 = '#FCE9E6';
+export const backgroundAccent200 = '#E0F5FC';
+export const backgroundAccent300 = '#FCF6E0';
+
+export const whiteFixed = '#FFFFF9';
+export const blackFixed = '#000000';
