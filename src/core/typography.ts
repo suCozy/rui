e16c93352a50a594dd740b3bfeee11b23ab13a37
JeongminSung <hellojeongmin@gmail.com>
2022-10-17 15:35:37 +0900

    PCWEB-8704 Typography

---
+++ src/core/typography.ts
@@ -0,0 +1,54 @@
+export const TYPOGRAPHY_STYLES = {
+  Headline2_B: {
+    fontSize: 24,
+    lineHeight: 34,
+    fontWeight: 600,
+  },
+  Headline3_B: {
+    fontSize: 20,
+    lineHeight: 26,
+    fontWeight: 600,
+  },
+  Headline4_B: {
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: 600,
+  },
+  Headline4_M: {
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: 400,
+  },
+  Body1_B: {
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: 600,
+  },
+  Body1_M: {
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: 400,
+  },
+  Body2_B: {
+    fontSize: 12,
+    lineHeight: 19,
+    fontWeight: 600,
+  },
+  Body2_M: {
+    fontSize: 12,
+    lineHeight: 19,
+    fontWeight: 400,
+  },
+  Caption_B: {
+    fontSize: 10,
+    lineHeight: 14,
+    fontWeight: 600,
+  },
+};
+
+/**
+ * {@link https://github.com/orioncactus/pretendard#html-1}
+ */
+
+export const FONT_FAMILY =
+  "Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif";
