b6d07823c60b5d4d5129ab5c21d78b595e9cfcce
JeongminSung <hellojeongmin@gmail.com>
2022-10-27 17:49:58 +0900

    PCWEB-8814 스토리북 설정

---
+++ src/components/Common/Pagination/Pagination.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Pagination } from '.';
+
+const meta: ComponentMeta<typeof Pagination> = {
+  title: 'Common/Pagination',
+  component: Pagination,
+  args: {
+    totalItemCount: 100,
+    pageRangeDisplayed: 10,
+    itemCountPerPage: 10,
+  },
+};
+
+export const Basic: ComponentStory<typeof Pagination> = (args) => {
+  const [pageNumber1, setPageNumber1] = React.useState(1);
+
+  return (
+    <Pagination
+      {...args}
+      onChangePage={(page) => {
+        setPageNumber1(page);
+      }}
+      activePage={pageNumber1}
+    />
+  );
+};
+
+export default meta;
