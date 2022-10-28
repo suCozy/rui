ac049862733ab86e6d3a6cce1ba5463bac83be31
JeongminSung <hellojeongmin@gmail.com>
2022-10-28 10:59:12 +0900

    PCWEB-8815 경로 추가 및 @radix-ui/react-checkbox 인스톨

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ export * from './assets';
 
 export * from './components/Buttons';
 export * from './components/Logos';
+export * from './components/Modals';
 export * from './components/Typography';
 export * from './components/Common/Flex';
 export * from './components/Common/Pagination';
-export * from './components/Modals';
+export * from './components/Control/Check';
