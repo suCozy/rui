07f46afb7b965dbbca0f2dc4a6eb1406e38cd263
JeongminSung <hellojeongmin@gmail.com>
2022-11-02 21:56:51 +0900

    PCWEB-8820 코드리뷰 반영

--- src/components/Controls/Control/ControlOptionIcon/index.tsx
+++ src/components/Controls/Control/ControlOptionIcon/index.tsx
@@ -1,8 +1,8 @@
 import { contents300, secondary100 } from 'colors/v3';
-import Icon, { IconName } from 'components/Icon';
-import { ControlChildrenType } from '../types';
+import { Icon, type IconName } from 'components/Icon';
+import type { ControlChildrenType, ControlOption } from '../types';
 
-const ICON_NAME = {
+const ICON_NAME: Record<ControlOption, Record<'on' | 'off', IconName>> = {
   check: { on: 'control_check_on_m', off: 'control_check_off_m' },
   checkbox: { on: 'control_checkbox_on_m', off: 'control_checkbox_off_m' },
   favorite: { on: 'control_bookmark_on_m', off: 'control_bookmark_off_m' },
@@ -14,11 +14,7 @@ function ControlOptionIcon({ checked, size, option }: ControlChildrenType) {
 
   return (
     <Icon
-      iconName={
-        checked
-          ? (ICON_NAME[option].on as IconName)
-          : (ICON_NAME[option].off as IconName)
-      }
+      iconName={checked ? ICON_NAME[option].on : ICON_NAME[option].off}
       width={iconSize}
       height={iconSize}
       aria-hidden="true"
