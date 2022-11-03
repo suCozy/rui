6b1c6844271eee4bcd4787846cba2bfb4370efbd
JeongminSung <hellojeongmin@gmail.com>
2022-11-03 10:06:02 +0900

    PCWEB-8855 Icon 컴포넌트 assets에 등록된 svg파일들 root의 index.ts 통해 아이콘 파일 가져오도록 경로 변경

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 export * as colors from './colors';
 export * as mixins from './mixins';
 
-export * from './assets';
+export * as Icons from './assets';
 
 export * from './components/Buttons/Button';
 export * from './components/Modals/Dialog';
 
 export * from './components/Logos';
-export * from './components/Modals';
 export * from './components/Typography';
 
 export * from './components/Common/Divider';
