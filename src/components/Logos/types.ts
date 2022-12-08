f6359977b78644e161d34149184af63b89acdd0b
JeongminSung <hellojeongmin@gmail.com>
2022-12-08 17:23:19 +0900

    PCWEB-9146 Env 라벨을 분리

---
+++ src/components/Logos/types.ts
@@ -0,0 +1,11 @@
+import { EnvType } from './EnvLabel';
+
+export interface RememberLogoProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  ref?: React.Ref<HTMLDivElement>;
+  className?: string;
+  color?: 'white' | 'black' | string;
+  height?: number;
+  env?: EnvType;
+  customElement?: React.ReactNode;
+}
