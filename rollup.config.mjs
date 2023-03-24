8fc177e7de05579ca868fae55da6ec00edcde3d5
kimjeongwonn <kimjeongwonn@gmail.com>
2023-03-24 16:42:20 +0900

    PCWEB-9656 fix: config통합 외
    - esm, cjs config를 통합하고 output으로 분리
    - 주석 추가
    - package.json도 그냥 복사로 추가

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,33 +9,18 @@ import fs from 'fs';
 import { promise as glob } from 'glob-promise';
 import path from 'path';
 import { defineConfig } from 'rollup';
-import generatePackageJson from 'rollup-plugin-generate-package-json';
 import typescript from 'rollup-plugin-typescript2';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { fileURLToPath } from 'url';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const BASE_DIR = './package';
+const PACKAGE_DIST_DIR = './package';
 const SOURCE_DIR = './src';
 
 const extensions = [...DEFAULT_EXTENSIONS, '.ts', '.tsx', '.json', '.mts'];
 const libraryEntryNames = ['legacy', 'icons'];
-
-const packageJsonGenerators = libraryEntryNames.map((entryName) => {
-  return generatePackageJson({
-    outputFolder: path.join(BASE_DIR, entryName),
-    baseContents: () => {
-      return {
-        main: './index.cjs.js',
-        module: './index.esm.js',
-        types: './index.d.ts',
-      };
-    },
-  });
-});
 
 const plugins = [
   typescript({
@@ -49,8 +34,8 @@ const plugins = [
     ],
     tsconfigOverride: {
       compilerOptions: {
-        mapRoot: path.join(process.cwd()),
-        declarationDir: path.join(BASE_DIR),
+        mapRoot: process.cwd(),
+        declarationDir: path.join(PACKAGE_DIST_DIR),
       },
     },
     useTsconfigDeclarationDir: true,
@@ -58,8 +43,14 @@ const plugins = [
   peerDepsExternal(),
   alias({
     entries: {
+      // tsconfig의 paths 옵션을 반영
       '@': path.join(__dirname, SOURCE_DIR),
     },
+  }),
+  nodeResolve({
+    extensions,
+    // esm을 먼저 읽도록 순서 변경
+    mainFields: ['main', 'module'],
   }),
   commonjs({
     sourceMap: true,
@@ -67,22 +58,38 @@ const plugins = [
   babel({
     sourceMap: true,
     extensions,
+    // babelHelper를 번들에 포함하지 않고 런타임에서 사용하는 시점에 불러오도록 설정
     babelHelpers: 'runtime',
     exclude: './node_modules/**/*',
   }),
   svgr({ exportType: 'named', typescript: true }),
   terser({ sourceMap: true }),
+  visualizer({ open: true }),
 ];
 
 export default async () => {
-  if (!fs.existsSync(BASE_DIR)) {
-    fs.mkdirSync(BASE_DIR);
+  // ./package 폴더가 없다면 생성
+  if (!fs.existsSync(PACKAGE_DIST_DIR)) {
+    fs.mkdirSync(PACKAGE_DIST_DIR);
   }
-  fs.cpSync(SOURCE_DIR, path.join(BASE_DIR, SOURCE_DIR), {
+
+  // storybook 관련 파일 제외하고 복사
+  fs.cpSync(SOURCE_DIR, path.join(PACKAGE_DIST_DIR, SOURCE_DIR), {
     recursive: true,
     filter: (source) => !source.includes('stories'),
   });
-  fs.copyFileSync('./tsconfig.json', path.join(BASE_DIR, 'tsconfig.json'));
+
+  // src에서 참조할 tsconfig 복사
+  fs.copyFileSync(
+    './tsconfig.json',
+    path.join(PACKAGE_DIST_DIR, 'tsconfig.json')
+  );
+
+  // package.json 복사
+  fs.copyFileSync(
+    './package.json',
+    path.join(PACKAGE_DIST_DIR, 'package.json')
+  );
 
   const entries = await glob(path.join(SOURCE_DIR, '**/index.{ts,tsx}'));
   const inputEntries = entries.reduce((obj, entry) => {
@@ -96,65 +103,13 @@ export default async () => {
   }, {});
 
   return defineConfig([
-    // cjs output
     {
-      plugins: [
-        nodeResolve({
-          extensions,
-          mainFields: ['main', 'module'],
-        }),
-        visualizer({ open: true }),
-        ...plugins,
-      ],
+      plugins,
       input: {
         index: path.join(SOURCE_DIR, '/index.ts'),
         ...inputEntries,
       },
-      output: {
-        dir: BASE_DIR,
-        sourcemap: true,
-        format: 'cjs',
-        entryFileNames: '[name].cjs.js',
-        chunkFileNames: 'chunks/[hash]/[name].cjs.js',
-        interop: 'auto',
-      },
-      external: [/@babel\/runtime/],
-    },
-    // esm output
-    {
-      plugins: [
-        nodeResolve({
-          extensions,
-        }),
-        ...plugins,
-        generatePackageJson({
-          inputFolder: './',
-          outputFolder: BASE_DIR,
-          baseContents: (options) => {
-            return options;
-          },
-        }),
-        ...packageJsonGenerators,
-      ],
-      input: {
-        index: path.join(SOURCE_DIR, '/index.ts'),
-        ...inputEntries,
-      },
-      output: [
-        {
-          dir: BASE_DIR,
-          sourcemap: true,
-          format: 'es',
-          entryFileNames: '[name].esm.js',
-          chunkFileNames: 'chunks/[hash]/[name].esm.js',
-          interop: 'auto',
-          manualChunks: (id) => {
-            if (id.endsWith('.svg')) {
-              return 'svgAssets';
-            }
-          },
-        },
-      ],
+      output: ['cjs', 'esm'].map(getOutputOption),
       external: [/@babel\/runtime/],
     },
   ]);
@@ -170,4 +125,20 @@ function getInputAlias(path) {
   }
 
   return `dist/${entryName}`;
+}
+
+function getOutputOption(format) {
+  return {
+    dir: PACKAGE_DIST_DIR,
+    sourcemap: true,
+    format,
+    entryFileNames: `[name].${format}.js`,
+    chunkFileNames: `chunks/[hash]/[name].${format}.js`,
+    interop: 'auto',
+    manualChunks: (id) => {
+      if (id.endsWith('.svg')) {
+        return 'svgAssets';
+      }
+    },
+  };
 }
