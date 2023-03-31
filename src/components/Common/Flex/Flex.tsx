0da3020a998f6024abc89b58c3b5472d7fbb305e
JeongminSung <hellojeongmin@gmail.com>
2023-03-31 16:46:18 +0900

    Revert " fix: Flex width/height/gap 세팅 number 허용"
    This reverts commit f9ca1d8f322d26de4c5293df6e8e7367a68bae0e.

--- src/components/Common/Flex/Flex.tsx
+++ src/components/Common/Flex/Flex.tsx
@@ -6,20 +6,17 @@ export interface FlexProps extends HTMLAttributes<HTMLDivElement> {
   direction?: CSSProperties['flexDirection'];
   justify?: CSSProperties['justifyContent'];
   wrap?: CSSProperties['flexWrap'];
-  gap?: string | number;
-  width?: string | number;
-  height?: string | number;
+  gap?: string;
+  width?: string;
+  height?: string;
 }
 
 const StyledFlex = styled.div<Partial<FlexProps>>`
   box-sizing: border-box;
   display: flex;
-  height: ${({ width = 'auto' }) =>
-    typeof width === 'number' ? `${width}px` : width};
-  height: ${({ height = 'auto' }) =>
-    typeof height === 'number' ? `${height}px` : height};
-
-  gap: ${({ gap = 0 }) => (typeof gap === 'number' ? `${gap}px` : gap)};
+  width: ${({ width = 'auto' }) => width};
+  height: ${({ height = 'auto' }) => height};
+  gap: ${({ gap = '0' }) => gap};
   align-items: ${({ align = 'center' }) => align};
   justify-content: ${({ justify = 'center' }) => justify};
   flex-direction: ${({ direction = 'row' }) => direction};
