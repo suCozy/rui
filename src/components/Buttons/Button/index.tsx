32c7733745419ef2bfd623d30ff00a58949e2af0
kimjeongwonn <kimjeongwonn@gmail.com>
2023-04-05 17:29:19 +0900

    RUI-1 feat: ref전달 및 memo

--- src/components/Buttons/Button/index.tsx
+++ src/components/Buttons/Button/index.tsx
@@ -1,34 +1,41 @@
+import type { ForwardRefRenderFunction } from 'react';
+import { forwardRef, memo } from 'react';
+
 import { Spinner } from '@/components/Common';
 
 import { SPINNER_SIZE_MAP } from './const';
 import { StyledButton } from './styles';
 import type { ButtonProps } from './types';
 
-export function Button({
-  children,
-  color,
-  size = 'small',
-  variant = 'primary',
-  layout = 'block',
-  isLoading = false,
-  icon,
-  iconPosition,
-  ...props
-}: ButtonProps) {
-  return (
-    <StyledButton
-      $color={color}
-      $size={size}
-      variant={variant}
-      layout={layout}
-      isLoading={isLoading}
-      aria-live="polite"
-      aria-busy={isLoading}
-      {...props}
-    >
-      {iconPosition === 'left' && icon}
-      {isLoading ? <Spinner size={SPINNER_SIZE_MAP[size]} /> : children}
-      {iconPosition === 'right' && icon}
-    </StyledButton>
-  );
-}
+const _Button: ForwardRefRenderFunction<HTMLButtonElement, ButtonProps> = (
+  {
+    children,
+    color,
+    size = 'small',
+    variant = 'primary',
+    layout = 'block',
+    isLoading = false,
+    icon,
+    iconPosition,
+    ...props
+  },
+  ref
+) => (
+  <StyledButton
+    $color={color}
+    $size={size}
+    variant={variant}
+    layout={layout}
+    isLoading={isLoading}
+    aria-live="polite"
+    aria-busy={isLoading}
+    ref={ref}
+    {...props}
+  >
+    {iconPosition === 'left' && icon}
+    {isLoading ? <Spinner size={SPINNER_SIZE_MAP[size]} /> : children}
+    {iconPosition === 'right' && icon}
+  </StyledButton>
+);
+
+export const Button = memo(forwardRef(_Button));
